57e8eb5fc93076b5694141cdc18c8882c5a06a63
kawasaki <yomitanmasa3523@gmail.com>
2020-07-07 15:34:39 +0900

    model作成　修正予定

--- app/Models/Output.php
+++ app/Models/Output.php
@@ -15,5 +15,17 @@ class Output extends Model
         return $this->belongsTo(User::class);
     }
 
-    
+    public function getOutput(Int $book_id)
+    {
+        return $this->with('user')->where('book_id', $book_id)->get();
+    }
+
+    public function commentStore(Int $user_id, Array $data)
+    {
+      $this->user_id = $user_id;
+      $this->book_id = $data['book_id'];
+      $this->text = $data['text'];
+      $this->save();
+      return;
+    }
 }
