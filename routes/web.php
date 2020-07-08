fdb8657c51fc344f0b1e8a1d6c8112d5015c9ca1
kawasaki <yomitanmasa3523@gmail.com>
2020-07-08 15:31:21 +0900

    user

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/books');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('books', 'booksController@index')->name('books.index');
+
+#####ユーザー
+Route::resource('users', 'UsersController',['only' => ['index', 'show']]);
