f509448c24e964dbd16b1a902ae990cc8440ecb9
kawasaki <yomitanmasa3523@gmail.com>
2020-07-22 16:27:00 +0900

    entity/table作成
    分割しました

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Book;
-use App\Models\Sentence;
-use App\Models\Tag;
+use App\Models\Entity\Book;
+use App\Models\Entity\Sentence;
+use App\Models\Entity\Tag;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
