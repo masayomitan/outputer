7299bc3e3ae96896940c6d092f9a7480986a8cac
kawasaki <yomitanmasa3523@gmail.com>
2020-07-22 20:42:33 +0900

    controller

--- app/Models/Favorites.php
+++ app/Models/Favorites.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favorites extends Model
 {
-    //
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+     //いいねしてるかの判定処理
+     public function isFavorite(Int $user_id, Int $sentence_id)
+     {
+         return (boolean) $this->where('user_id', $user_id)->where('sentence_id', $sentence_id)->first();
+     }
+
+     public function favoriteStore(Int $user_id, Int $sentence_id)
+     {
+        $this->user_id = $user_id;
+        $this->sentence_id = $sentence_id;
+        $this->save();
+        return;
+     }
+
+     public function favoriteDestroy(Int $favorite_id)
+    {
+        return $this->where('id', $favorite_id)->delete();
+    }
+
+
 }
