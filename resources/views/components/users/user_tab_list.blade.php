bf366414b85e6e259d8e94bb7c021b8e6162235f
kawasaki <yomitanmasa3523@gmail.com>
2020-08-08 11:08:41 +0900

    userTab変更

--- resources/views/components/users/user_tab_list.blade.php
+++ resources/views/components/users/user_tab_list.blade.php
@@ -1,13 +1,26 @@
 
 
-    <div class="tab-info-box">
-        <div class="tab-info-box-num-list">
+    <div class="tab-wrap">
+        <div class="tab-content">
             @foreach($tab_info_list as $tab_text => $tab_info)
-            <div class="tab-info-box-num-list-each">
-                    <a href="{{ $tab_info['link'] }}" class="tab-info-button">
-                    <div class="tab-info-num">{{$tab_text}}</div>
-                    </a>
+            <div class="tab-num">
+                    <a href="{{ $tab_info['link'] }}">{{$tab_text}}</a>
             </div>
             @endforeach
         </div>
+        <div class="tab-name">
+            <div class="tab-code">
+                投稿数
+            </div>
+            <div class="tab-code">
+                いいね数
+            </div>
+            <div class="tab-code">
+                フォロー
+            </div>
+            <div class="tab-code">
+                フォロワー
+            </div>
+        </div>
     <div>
+
