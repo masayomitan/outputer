773445cf76550314bc82b3de8964ffa2209eb4b7
kawasaki <yomitanmasa3523@gmail.com>
2020-08-17 13:39:45 +0900

    user edit 機能 画像nullのエラー修正

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,183 +19,178 @@ use Illuminate\Support\Facades\Validator;
 
 class UsersController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index(User $user, Request $request)
-  {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(User $user, Request $request)
+    {
 
-    if (auth()->user()) {
-      //ログインしている場合、自分以外のユーザー情報を取得
-      $all_users = $user->getAllUsers(auth()->user()->id);
-      return view('users.index', [
-        'all_users' => $all_users
-      ]);
-    } else {
-      //ログインしていない場合、全てのユーザー情報を取得
-      $all_users = $user->getAllUsers(auth()->user());
-      return view('users.index', [
-        'all_users' => $all_users
-      ]);
-    }
-  }
-
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function create()
-  {
-    //
-  }
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(Request $request)
-  {
-    //
-  }
-
-
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function show(User $user, Book $book, Sentence $sentence, Request $request)
-  {
-    if (!isset($request["status"])) {
-      $request["status"] = 0;
+        if (auth()->user()) {
+        //ログインしている場合、自分以外のユーザー情報を取得
+        $all_users = $user->getAllUsers(auth()->user()->id);
+        return view('users.index', [
+            'all_users' => $all_users
+        ]);
+        } else {
+        //ログインしていない場合、全てのユーザー情報を取得
+        $all_users = $user->getAllUsers(auth()->user());
+        return view('users.index', [
+            'all_users' => $all_users
+        ]);
+        }
     }
 
-    #ログインユーザーじゃないユーザーが下書きページに遷移した際、リダイレクトして閲覧を防ぐ
-    if ($request["status"] == 1) {
-      $is_self_sentence = $user->isSelfSentence($request, $user);
-      if (!$is_self_sentence) {
-        return redirect($request->path());
-      }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
     }
 
-    $timelines = $sentence->getUserTimeLine($user->id, $request["status"]);
-    $user_info_list = $user->getUserInfoList();
-    $user_info_list["timelines"] = $timelines;
-    $user_info_list["sentence_status_list"] = ['公開中', '下書き'];
-    $user_info_list["request_status_id"] = $request["status"];
-
-    return view('users.show', $user_info_list);
-  }
-
-
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function edit(User $user)
-  {
-    $user = auth()->user();
-    return view('users.edit', ['user' => $user]);
-  }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
 
 
-  public function update(Request $request, User $user)
-  {
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user, Book $book, Sentence $sentence, Request $request)
+    {
+        if (!isset($request["status"])) {
+        $request["status"] = 0;
+        }
 
-    $user = User::find(Auth::user()->id);
+        #ログインユーザーじゃないユーザーが下書きページに遷移した際、リダイレクトして閲覧を防ぐ
+        if ($request["status"] == 1) {
+        $is_self_sentence = $user->isSelfSentence($request, $user);
+        if (!$is_self_sentence) {
+            return redirect($request->path());
+        }
+        }
 
-    $file_name = $request->file('profile_image')->getClientOriginalName();
-    $request->file('profile_image')->storeAs('public/profile_image',$file_name);
+        $timelines = $sentence->getUserTimeLine($user->id, $request["status"]);
+        $user_info_list = $user->getUserInfoList();
+        $user_info_list["timelines"] = $timelines;
+        $user_info_list["sentence_status_list"] = ['公開中', '下書き'];
+        $user_info_list["request_status_id"] = $request["status"];
 
-    $data = $request->all();
-    $validator = Validator::make($data, [
-      #0-9,英数字,記号の_のみだけ登録できるよう設定
-      'screen_name' => ['required', 'regex:/^(\w)+$/', 'max:50', Rule::unique('users')->ignore($user->id)],
-      'name' => ['required', 'string', 'max:255'],
-      'profile_image' => ['file', 'image', 'mimes:jpeg,png,jpg', 'max:2048'],
-      'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($user->id)]
-    ]);
+        return view('users.show', $user_info_list);
+    }
 
 
-    $validator->validate();
-    $user->userUpdate($data, $file_name);
-    return redirect('users/' . $user->id);
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        $user = auth()->user();
+        return view('users.edit', ['user' => $user]);
+    }
 
-  }
 
-  /**
+    public function update(Request $request, User $user)
+    {
+        $user = User::find(Auth::user()->id);
+        $data = $request->all();
+        if(isset($data["profile_image"])){
+            $file_name = $request->file('profile_image')->getClientOriginalName();
+            $request->file('profile_image')->storeAs('public/profile_image',$file_name);
+            $data["profile_image"] = $file_name;
+        }
+        $validator = Validator::make($data, [
+            'name' => ['required', 'string', 'max:50'],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($user->id),
+            'self_introduction' => ['required', 'textarea', 'max:200']]
+        ]);
+            $validator->validate();
+            $user->userUpdate($data);
+        return redirect('users/' . $user->id);
+    }
+
+    /**
    * Remove the specified resource from storage.
    *
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function destroy($id)
-  {
+    public function destroy($id)
+    {
     //
-  }
+    }
 
    // フォロー
-   public function follow(User $user)
-   {
-       $follower = auth()->user();
-       // フォローしているか
-       $is_following = $follower->isFollowing($user->id);
-       if(!$is_following) {
-           // フォローしていなければフォローする
-           $follower->follow($user->id);
-           return back();
-       }
-   }
+    public function follow(User $user)
+    {
+        $follower = auth()->user();
+        // フォローしているか
+        $is_following = $follower->isFollowing($user->id);
+        if(!$is_following) {
+            // フォローしていなければフォローする
+            $follower->follow($user->id);
+            return back();
+        }
+    }
 
    // フォロー解除
-   public function unfollow(User $user)
-   {
-       $follower = auth()->user();
-       // フォローしているか
-       $is_following = $follower->isFollowing($user->id);
-       if($is_following) {
-           // フォローしていればフォローを解除する
-           $follower->unfollow($user->id);
-           return back();
-       }
-   }
+    public function unfollow(User $user)
+    {
+        $follower = auth()->user();
+        // フォローしているか
+        $is_following = $follower->isFollowing($user->id);
+        if($is_following) {
+            // フォローしていればフォローを解除する
+            $follower->unfollow($user->id);
+            return back();
+        }
+    }
 
    //フォローリスト表示 :example(users/{id}/following)
-   public function following(User $user)
-   {
-     $following_users = $user->getFollowingUsers($user->id);
-     $user_info_list = $user->getUserInfoList();
-     $user_info_list["all_users"] = $following_users;
-     //view先で条件分岐
-     return view('users.follow', $user_info_list);
-   }
+    public function following(User $user)
+    {
+        $following_users = $user->getFollowingUsers($user->id);
+        $user_info_list = $user->getUserInfoList();
+        $user_info_list["all_users"] = $following_users;
+        //view先で条件分岐
+        return view('users.follow', $user_info_list);
+    }
 
 
-   //フォロワーリスト表示 :example(users/{id}/followers)
-   public function followers(User $user)
-   {
-     $followers = $user->getFollowerUsers($user->id);
-     $user_info_list = $user->getUserInfoList();
-     $user_info_list["all_users"] = $followers;
-     //view先で条件分岐
-     return view('users.follow', $user_info_list);
-   }
+    //フォロワーリスト表示 :example(users/{id}/followers)
+    public function followers(User $user)
+    {
+        $followers = $user->getFollowerUsers($user->id);
+        $user_info_list = $user->getUserInfoList();
+        $user_info_list["all_users"] = $followers;
+        //view先で条件分岐
+        return view('users.follow', $user_info_list);
+    }
 
-   //いいねした記事リスト表示 :example(users/{id}/favorites)
-   public function favorites(User $user, Sentence $sentence)
-   {
-     $timelines = $sentence->getFavoriteSentences($user->id);
-     $user_info_list = $user->getUserInfoList();
-     $user_info_list["timelines"] = $timelines;
-     return view('users.favorites', $user_info_list);
-   }
+    //いいねした記事リスト表示 :example(users/{id}/favorites)
+    public function favorites(User $user, Sentence $sentence)
+    {
+        $timelines = $sentence->getFavoriteSentences($user->id);
+        $user_info_list = $user->getUserInfoList();
+        $user_info_list["timelines"] = $timelines;
+        return view('users.favorites', $user_info_list);
+    }
 
- }
+    }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,8 +9,6 @@
                                 <p class="mb-0">{{ $user->name }}</p>
                                 <a href="{{ url('users/' .$user->id) }}" class="text-secondary">{{ $user->screen_name }}</a>
                             </div>
-                            {{-- <img  src="{{ asset('storage/book_image/' . $book->book_image) }}" alt="{{ $book->book_image }}" width="100px" class="w-100"> --}}
-
                             @if (auth()->user()->isFollowed($user->id))
                                 <div class="px-2">
                                     <span class="px-1 bg-secondary text-light">フォローされています</span>
@@ -24,25 +22,14 @@
                                     <form action="{{ route('users.unfollow', $user->id) }}" method="POST">
                                         {{ csrf_field() }}
                                         {{ method_field('DELETE') }}
-
                                         <button type="submit" class="btn btn-danger">フォロー解除</button>
                                     </form>
                                 @else
                                     <form action="{{ route('users.follow', $user->id) }}" method="POST">
                                         {{ csrf_field() }}
-
                                         <button type="submit" class="btn btn-primary">フォローする</button>
                                     </form>
                                 @endif
-                            </div>
-                        </div>
-                    </div>
                 @endforeach
-            </div>
-        </div>
-        <div class="my-4 d-flex justify-content-center">
             {{ $all_users->links() }}
-        </div>
-    </div>
-{{-- @endsection --}}
-
+@endsection
