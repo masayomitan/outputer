7b0215269acf7b6fa01b163ba1b864ebc9820a69
kawasaki <yomitanmasa3523@gmail.com>
2020-08-22 13:28:32 +0900

    middleware 追記

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return redirect('/books');
-// });
+Route::get('/', function () {
+    return redirect('/books');
+});
 
 
 
