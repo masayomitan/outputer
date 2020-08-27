e2deb426ccf75f9a9acabee0e20858492a2ac678
kawasaki <yomitanmasa3523@gmail.com>
2020-08-27 11:00:55 +0900

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,7 +47,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/public/book_image/'),
+            'root' => storage_path('app'),
         ],
 
         'public' => [
