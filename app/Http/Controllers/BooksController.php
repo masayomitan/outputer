09deaa3967ea7f94bbb934dbabff0961657e11f3
kawasaki <yomitanmasa3523@gmail.com>
2020-08-27 12:21:31 +0900

    Update BooksController.php

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -68,10 +68,11 @@ class BooksController extends Controller
     {
         $user = auth()->user();
         $data = $request->all();
-        $file_name = $request->file('book_image');
+        // $file_name = $request->file('book_image')->getClientOriginalName();
         // $request->file('book_image')->storeAs('/public/book_image',$file_name);
         // $data["book_image"] = $file_name;
-
+        
+        $file_name = $request->file('book_image');
         $book_image = Storage::disk('s3')->putFile('book_image', $file_name, 'public');
         $data["book_image"] = Storage::disk('s3')->url($book_image);
 
