7669f869c4281826d070c7273b26a196e844e409
kawasaki <yomitanmasa3523@gmail.com>
2020-08-27 13:09:02 +0900

    Update timeline_sentence_list.blade.php

--- resources/views/components/users/timeline_sentence_list.blade.php
+++ resources/views/components/users/timeline_sentence_list.blade.php
@@ -45,7 +45,7 @@
         </div>
 
         <div class="timeline-user-box">
-            <img class="timeline-user-image" src="{{ asset('storage/profile_image/' .$timeline->user->profile_image)}}">
+            <img class="timeline-user-image" src="{{ $timeline->user->profile_image)}}">
             <p class="timeline-user-name">{!! nl2br(e(Str::limit($timeline->user->name, 16))) !!}</p>
             <p class="timeline-date">{{ $timeline->created_at->format('Y-m-d H:i') }}</p>
                 @if (isset(auth()->user()->id))
