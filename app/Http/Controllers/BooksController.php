4652c088057632012df2b2ce9f0d3545689902fa
kawasaki <yomitanmasa3523@gmail.com>
2020-08-27 11:33:37 +0900

    Update BooksController.php

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -69,7 +69,7 @@ class BooksController extends Controller
         $user = auth()->user();
         $data = $request->all();
         $file_name = $request->file('book_image')->getClientOriginalName();
-        $request->file('book_image')->storeAs('/public/book_image',$file_name);
+        // $request->file('book_image')->storeAs('/public/book_image',$file_name);
 
         $book_image = Storage::disk('s3')->putFile('book_image', $file_name, 'public');
 
