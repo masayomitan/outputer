e7917ccf2a39c07255c58d75e45ce6d39e4f465c
kawasaki <yomitanmasa3523@gmail.com>
2020-08-27 11:16:09 +0900

    Update BooksController.php

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -71,10 +71,9 @@ class BooksController extends Controller
         $file_name = $request->file('book_image')->getClientOriginalName();
         $request->file('book_image')->storeAs('/public/book_image',$file_name);
 
-        $contents = Storage::get('/public/book_image/',$file_name);
-        $book_image = Storage::disk('s3')->putFile('book_image', $contents, 'public');
+        $book_image = Storage::disk('s3')->putFile('book_image', $file_name, 'public');
 
-        $data["book_image"] = Storage::disk('s3')->url($book_image);
+        $data["book_image"]  = Storage::disk('s3')->url($book_image);
 
         $validator = Validator::make($data,[
             'title' => ['string', 'max:30'],
