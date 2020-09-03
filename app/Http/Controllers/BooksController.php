e3c75116f33ff94b9850c46fddeb7993d6f0c877
kawasaki <yomitanmasa3523@gmail.com>
2020-09-04 00:53:46 +0900

    array_filterで配列内のnull削除

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -75,15 +75,18 @@ class BooksController extends Controller
         $book_image = Storage::disk('s3')->putFile('book_image', $file_name, 'public');
         $data["book_image"] = Storage::disk('s3')->url($book_image);
 
+        $data["tags"] = array_filter($data["tags"]);
         $rules = [
             'title' =>  ['string', 'max:50'],
             'author' => ['string', 'max:30'],
             'tags.*' =>   ['string', 'max:10'],
         ];
+
+        // dd($rules);
         $messages = [
             'title.max'        => 'タイトルは:max文字以内で入力してください。',
             'author.max'       => '著者名は:max文字以内で入力してください。',
-            'tags.*.max'         => 'タグは10文字以内で入力してください',
+            'tags.*.max'       => 'タグは10文字以内で入力してください',
         ];
 
         $validator = Validator::make($data, $rules, $messages);
@@ -95,10 +98,10 @@ class BooksController extends Controller
         if(empty($tag_name)) {
             return redirect('/books')->with(['validated'=>$validated]);
         } else {
-            $tag->tagStore($data["tags"]);       //タグ登録メソッド
-            $tags = array_filter($data["tags"]); //array_filterで連想配列の空チェック,空削除でエラー防ぐ
-            $tag_ids = $tag->getTagIds($tags);   //$tagテーブルに挿入した値の名前からidを取得し中間テーブルへ
-            $book->bookTagSync($tag_ids);                //中間テーブルにidを設置
+            $tag->tagStore($data["tags"]);        //タグ登録メソッド
+            $tags = array_filter($data["tags"]);  //array_filterで連想配列の空チェック,空削除でエラー防ぐ
+            $tag_ids = $tag->getTagIds($tags);    //$tagテーブルに挿入した値の名前からidを取得し中間テーブルへ
+            $book->bookTagSync($tag_ids);         //中間テーブルにidを設置
         }
 
         return redirect('/books')->with(['validated'=>$validated]);
