fbf735b93c4bb10340dbeca7f4bc647477404d58
kawasaki <yomitanmasa3523@gmail.com>
2020-09-12 10:00:53 +0900

    viewport

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
     <!-- Scripts -->
     <script src="{{ secure_asset('js/app.js') }}" defer></script>
 
+
+    <meta name=”viewport” content=”width=device-width, initial-scale=1”>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
