fdc0baa1eaeb86f1e7ebfb3aab7d9d4d1b28bf08
kawasaki <yomitanmasa3523@gmail.com>
2020-09-20 08:19:42 +0900

    Update 2020_09_20_075925_change_users_table_add_oauth_columns.php

--- database/migrations/2020_09_20_075925_change_users_table_add_oauth_columns.php
+++ database/migrations/2020_09_20_075925_change_users_table_add_oauth_columns.php
@@ -26,7 +26,7 @@ class ChangeUsersTableAddOauthColumns extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('avatar');
+            //
         });
     }
 }
