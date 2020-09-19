9ea89628663d4c58e8fe980079ccdb7a4c1a6a71
kawasaki <yomitanmasa3523@gmail.com>
2020-09-20 08:14:25 +0900

    Update 2020_09_20_075925_change_users_table_add_oauth_columns.php

--- database/migrations/2020_09_20_075925_change_users_table_add_oauth_columns.php
+++ database/migrations/2020_09_20_075925_change_users_table_add_oauth_columns.php
@@ -15,7 +15,6 @@ class ChangeUsersTableAddOauthColumns extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('email')->nullable()->change();
-
         });
     }
 
